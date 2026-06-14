6e182df89bd0490c36813cf4ac7325ee73079c22
Noah <152039306+NDCSwift@users.noreply.github.com>
2026-06-14 15:06:27 -0400

    Update YouTube handle to @NoahDoesCoding

--- AudioRecorderDemo2/AudioRecorderDemo2App.swift
+++ AudioRecorderDemo2/AudioRecorderDemo2App.swift
@@ -35,7 +35,7 @@
     //  File: AudioRecorderDemo2App.swift
     //  Created by Noah Carpenter
     //  🐱 Follow me on YouTube! 🎥
-    //  https://www.youtube.com/@NoahDoesCoding97
+    //  https://www.youtube.com/@NoahDoesCoding
     //  Like and Subscribe for coding tutorials and fun! 💻✨
     //  Fun Fact: Cats have five toes on their front paws, but only four on their back paws! 🐾
     //  Dream Big, Code Bigger
